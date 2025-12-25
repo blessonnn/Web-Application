74ba8db8ca8da5ff0ebfb4d6d1c09e31c8e1172a
blessonnn <jeyk7402@gmail.com>
2025-12-25 23:27:36 +0530

    simple web app using js

---
+++ passenger-count/index.js
@@ -0,0 +1,14 @@
+
+// document.getElementById("count").innerText=5
+let count=0
+console.log(count)
+function increment() {
+    console.log("You clicked the increment button!")
+}
+function decrement() {
+    console.log("You clicked the decrement button!")
+}
+
+
+
+
